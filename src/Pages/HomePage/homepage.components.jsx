f6b7b00be5b147ecd41c58bfef934716010bacc4
tech-911 <babatundeolatunji74@gmail.com>
2021-10-20 23:16:08 +0100

    updating...

--- src/Pages/HomePage/homepage.components.jsx
+++ src/Pages/HomePage/homepage.components.jsx
@@ -30,8 +30,96 @@ function HomePage() {
         <h1 id="h1">Discover things to do</h1>
         <Discover />
       </div>
-      <div className="HomePage-firstFooter">im the first footer</div>
-      <div className="HomePage-secondFooter">im a footer</div>
+      <div className="HomePage-firstFooter">
+        <div className="one">
+          <b>ABOUT</b>
+          <p>
+            How Airbnb works
+            <br />
+            Newsroom
+            <br />
+            Investors
+            <br />
+            Airbnb Plus
+            <br />
+            Airbnb Luxe
+            <br />
+            HotelTonight
+            <br />
+            Airbnb for Work
+            <br />
+            Made possible by Hosts
+            <br />
+            Careers
+            <br />
+            Founders' Letter
+          </p>
+        </div>
+
+        <div className="two">
+          <b>COMMUNITY</b>
+          <p>
+            Diversity & Belonging
+            <br />
+            Against Discrimination
+            <br />
+            Accessibility
+            <br />
+            Airbnb Associates
+            <br />
+            Host Afghan refugees
+            <br />
+            Guest Referrals
+            <br />
+            Gift cards
+            <br />
+            Airbnb.org
+          </p>
+        </div>
+
+        <div className="three">
+          <b>SUPPORT</b>
+          <p>
+            How Airbnb works
+            <br />
+            Our COVID-19 Response
+            <br />
+            Help Center
+            <br />
+            Cancellation options
+            <br />
+            Neighborhood Support
+            <br />
+            Trust & Safety
+          </p>
+        </div>
+        <div className="four">
+          <b>HOST</b>
+          <p>
+            Diversity & Belonging
+            <br />
+            Against Discrimination
+            <br />
+            Accessibility
+            <br />
+            Airbnb Associates
+            <br />
+            Host Afghan refugees
+            <br />
+            Guest Referrals
+            <br />
+            Gift cards
+            <br />
+            Airbnb.org
+          </p>
+        </div>
+      </div>
+      <div className="HomePage-secondFooter">
+        <div className="footer-left">
+          ©2021 Airbnb, Inc. · Privacy · Terms · Sitemap
+        </div>
+        <div className="footer-right"></div>
+      </div>
     </div>
   );
 }
