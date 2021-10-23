8a452cc98a12bb9731aaece13298446626ff9242
tech-911 <babatundeolatunji74@gmail.com>
2021-10-23 22:27:21 +0100

    done with home page ui

--- src/Pages/HomePage/homepage.components.jsx
+++ src/Pages/HomePage/homepage.components.jsx
@@ -5,6 +5,10 @@ import btn from "../../assets/I’m flexible.png";
 import LiveView from "../../Components/live-anywhere-component/live-anywhere.view";
 import TryHosting from "../../Components/tryhosting/tryhosting.components";
 import Discover from "../../Components/Discover/discover";
+import IconsFacebook from "../../assets/icons/facebook.png";
+import IconsLanguage from "../../assets/icons/language_24px.png";
+import Iconstwitter from "../../assets/icons/twitter.png";
+import Iconsinstagram from "../../assets/icons/facebook.png";
 
 function HomePage() {
   return (
@@ -31,94 +35,102 @@ function HomePage() {
         <Discover />
       </div>
       <div className="HomePage-firstFooter">
-        <div className="one">
-          <b>ABOUT</b>
-          <p>
-            How Airbnb works
-            <br />
-            Newsroom
-            <br />
-            Investors
-            <br />
-            Airbnb Plus
-            <br />
-            Airbnb Luxe
-            <br />
-            HotelTonight
-            <br />
-            Airbnb for Work
-            <br />
-            Made possible by Hosts
-            <br />
-            Careers
-            <br />
-            Founders' Letter
-          </p>
-        </div>
+        <div className="HomePage-firstFooter-child">
+          <div className="one">
+            <b>ABOUT</b>
+            <p>
+              How Airbnb works
+              <br />
+              Newsroom
+              <br />
+              Investors
+              <br />
+              Airbnb Plus
+              <br />
+              Airbnb Luxe
+              <br />
+              HotelTonight
+              <br />
+              Airbnb for Work
+              <br />
+              Made possible by Hosts
+              <br />
+              Careers
+              <br />
+              Founders' Letter
+            </p>
+          </div>
 
-        <div className="two">
-          <b>COMMUNITY</b>
-          <p>
-            Diversity & Belonging
-            <br />
-            Against Discrimination
-            <br />
-            Accessibility
-            <br />
-            Airbnb Associates
-            <br />
-            Host Afghan refugees
-            <br />
-            Guest Referrals
-            <br />
-            Gift cards
-            <br />
-            Airbnb.org
-          </p>
-        </div>
+          <div className="two">
+            <b>COMMUNITY</b>
+            <p>
+              Diversity & Belonging
+              <br />
+              Against Discrimination
+              <br />
+              Accessibility
+              <br />
+              Airbnb Associates
+              <br />
+              Host Afghan refugees
+              <br />
+              Guest Referrals
+              <br />
+              Gift cards
+              <br />
+              Airbnb.org
+            </p>
+          </div>
 
-        <div className="three">
-          <b>SUPPORT</b>
-          <p>
-            How Airbnb works
-            <br />
-            Our COVID-19 Response
-            <br />
-            Help Center
-            <br />
-            Cancellation options
-            <br />
-            Neighborhood Support
-            <br />
-            Trust & Safety
-          </p>
-        </div>
-        <div className="four">
-          <b>HOST</b>
-          <p>
-            Diversity & Belonging
-            <br />
-            Against Discrimination
-            <br />
-            Accessibility
-            <br />
-            Airbnb Associates
-            <br />
-            Host Afghan refugees
-            <br />
-            Guest Referrals
-            <br />
-            Gift cards
-            <br />
-            Airbnb.org
-          </p>
+          <div className="three">
+            <b>SUPPORT</b>
+            <p>
+              How Airbnb works
+              <br />
+              Our COVID-19 Response
+              <br />
+              Help Center
+              <br />
+              Cancellation options
+              <br />
+              Neighborhood Support
+              <br />
+              Trust & Safety
+            </p>
+          </div>
+          <div className="four">
+            <b>HOST</b>
+            <p>
+              Diversity & Belonging
+              <br />
+              Against Discrimination
+              <br />
+              Accessibility
+              <br />
+              Airbnb Associates
+              <br />
+              Host Afghan refugees
+              <br />
+              Guest Referrals
+              <br />
+              Gift cards
+              <br />
+              Airbnb.org
+            </p>
+          </div>
         </div>
       </div>
       <div className="HomePage-secondFooter">
         <div className="footer-left">
-          ©2021 Airbnb, Inc. · Privacy · Terms · Sitemap
+          <b> ©2021 Airbnb, Inc. · Privacy · Terms · Sitemap</b>
         </div>
-        <div className="footer-right"></div>
+        <div className="footer-right">
+          <img src={IconsLanguage} alt="" />
+          <b>English (US) $USD</b>
+          <img src={IconsFacebook} alt="" />
+          <img src={Iconstwitter} alt="" />
+          <img src={Iconsinstagram} alt="" />
+        </div>
       </div>
     </div>
   );
